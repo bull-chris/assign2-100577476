9a8d09e007fd4e34f6c817d2cf390059169f0c55
Chris Bull <christopher.bull@dcmail.ca>
2021-11-02 13:19:10 -0400

    Updated style names in functions.php
    Parent style name now matches with parent correctly and child theme name updated with -style

--- themes/lifewithhockeychrisbull-child/functions.php
+++ themes/lifewithhockeychrisbull-child/functions.php
@@ -8,10 +8,10 @@ function enqueue_life_with_hockey_theme() {
     $lifeWithHockey = wp_get_theme();
 
     //enqueue the twenty twenty one parent theme and its styling
-    wp_enqueue_style('twentytwentyone', get_template_directory_uri() . '/style.css', array(), $lifeWithHockey->parent()->get('Version'));
+    wp_enqueue_style('twenty-twenty-one-style', get_template_directory_uri() . '/style.css', array(), $lifeWithHockey->parent()->get('Version'));
 
     //enqueue the lifewithhockeychrisbull-child theme and its styling, with dependencies from the twenty twenty-one parent
-    wp_enqueue_style('lifewithhockeychrisbull-child', get_stylesheet_uri(), array('twentytwentyone'), $lifeWithHockey->get('Version'));
+    wp_enqueue_style('lifewithhockeychrisbullchild-style', get_stylesheet_uri(), array('twenty-twenty-one-style'), $lifeWithHockey->get('Version'));
 }
 
 //add the enqueue_life_with_hockey_theme function as the action for wordpress enqueue scripts to perform
