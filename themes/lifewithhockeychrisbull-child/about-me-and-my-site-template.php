5cee273204a6c5fdbb408e57fd1ce51ae414a717
Chris Bull <christopher.bull@dcmail.ca>
2021-12-05 01:28:26 -0500

    Added basic display for the About me and my Site page
    Code added to display content on the About me and my Site page

--- themes/lifewithhockeychrisbull-child/about-me-and-my-site-template.php
+++ themes/lifewithhockeychrisbull-child/about-me-and-my-site-template.php
@@ -1,5 +1,14 @@
 <?php 
 /* Template Name: Life with Hockey About me and my Site Page */ 
 
+get_header();
+
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+endwhile;
+
+get_footer();
 
 ?>
