4b11fa7501a3bbe0efdf77a21ce143700f908f5a
Chris Bull <christopher.bull@dcmail.ca>
2021-12-05 00:25:31 -0500

    Life with hockey styling css file created and enqueued in functions.php
    Created a new css folder to override stylings for the child theme, and enqueue the css file in the functions.php file

--- themes/lifewithhockeychrisbull-child/functions.php
+++ themes/lifewithhockeychrisbull-child/functions.php
@@ -12,6 +12,9 @@ function enqueue_life_with_hockey_theme() {
 
     //enqueue the lifewithhockeychrisbull-child theme and its styling, with dependencies from the twenty twenty-one parent
     wp_enqueue_style('lifewithhockeychrisbullchild-style', get_stylesheet_uri(), array('twenty-twenty-one-style'), $lifeWithHockey->get('Version'));
+
+    //enqueue the life-with-hockey-styling css file to use with the life with hockey child theme
+    wp_enqueue_style('lifewithhockeychrisbullchild-styling-overrides', get_stylesheet_directory_uri() . '/assets/css/life-with-hockey-styling.css', array('twenty-twenty-one-style'), $lifeWithHockey->get('Version'));
 }
 
 //add the enqueue_life_with_hockey_theme function as the action for wordpress enqueue scripts to perform
